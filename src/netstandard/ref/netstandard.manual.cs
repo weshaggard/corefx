2e2dcb306f8b57ca7f802ae14e914ea80c0b6c3e
Wes Haggard <Wes.Haggard@microsoft.com>
2016-08-18 12:39:51 -0700

    Initial netstandard ref project
    Generated individual cs genapi on the files from the Xamarin.iOS.
    
    A couple places had //REVIEW where I needed to comment out stuff
    
    system.drawing.cs was created manually by taking the System.Drawing
    namespaces in OpenTK and Xamarian.iOS assemblies.
    
    These are mostly unmodified based on our plans to clean up.

---
+++ src/netstandard/ref/netstandard.manual.cs
@@ -0,0 +1,37 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// ------------------------------------------------------------------------------
+// Changes to this file must follow the http://aka.ms/api-review process.
+// ------------------------------------------------------------------------------
+
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+namespace System
+{
+    public partial struct Single
+    {
+        public const float MinValue = (float)-3.40282346638528859e+38;
+        public const float Epsilon = (float)1.4e-45;
+        public const float MaxValue = (float)3.40282346638528859e+38;
+        public const float PositiveInfinity = (float)1.0 / (float)0.0;
+        public const float NegativeInfinity = (float)-1.0 / (float)0.0;
+        public const float NaN = (float)0.0 / (float)0.0;
+    }
+    public partial struct Double
+    {
+        public const double MinValue = -1.7976931348623157E+308;
+        public const double MaxValue = 1.7976931348623157E+308;
+
+        // Note Epsilon should be a double whose hex representation is 0x1
+        // on little endian machines.
+        public const double Epsilon = 4.9406564584124654E-324;
+        public const double NegativeInfinity = (double)-1.0 / (double)(0.0);
+        public const double PositiveInfinity = (double)1.0 / (double)(0.0);
+        public const double NaN = (double)0.0 / (double)0.0;
+    }
+}
